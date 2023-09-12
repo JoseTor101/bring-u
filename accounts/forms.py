53b4bd6a13293b5df92c3f9f6b2410a97e0d6f14
josetor101 <josetordecilla800@gmail.com>
2023-09-12 12:13:01 -0500

    Changes in project documentation routes

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,8 +3,8 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import UserProfile
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=200, help_text='Required')
+    #email = forms.EmailField(max_length=200, help_text='Required')
     class Meta:
         model = UserProfile
-        fields = ('username', 'email', 'name', 'last_name', 'tel', 'profile_pic', 'password1', 'password2')
+        fields = ('username', 'email', 'first_name', 'last_name', 'tel', 'profile_pic', 'password1', 'password2')
     
