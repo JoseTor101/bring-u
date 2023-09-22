b1c732096bdf0dc4b74ac8ebb854515fdaf5cc72
Crissitx <camilocardenas2705@gmail.com>
2023-09-22 15:39:51 -0500

    AI2.py

--- AI/AI2.py
+++ AI/AI2.py
@@ -2,5 +2,7 @@ from PIL import Image
 import pytesseract
 import os
 
-path = "AI/media/test.png"
-print(pytesseract.image_to_string(Image.open(path)))
+path = "AI/media/test2.png"
+config = r"--psm 11 --oem 3 -l spa"
+
+print(pytesseract.image_to_string(Image.open(path), config=config))
