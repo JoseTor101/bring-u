270a6d1ab15bf1ca8810aa51be584f79f323d4a2
josetor101 <josetordecilla800@gmail.com>
2023-09-30 15:59:46 -0500

    Basics of chat system made

--- chat/urls.py
+++ chat/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
- path('', views.Chats, name='chats'),
- path('conversation/<int:id_chat>/', views.Conversation, name='conversation'),
+    path('', views.Chats, name='chats'),
+    path('conversation/<int:id_chat>/', views.Conversation, name='conversation'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -2,12 +2,69 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Chat, Message  # Import your models
 from accounts.models import UserProfile  # Import your UserProfile model
 from django.utils import timezone
+from django.http import JsonResponse
+import json
 
 def Chats(request):
-    # Your code to fetch and display a list of chats
-    return render(request, 'chats.html')
+    user = request.user
+    current_user = UserProfile.objects.get(username=user)
+
+    chats = []
+    if current_user: 
+        chats = Chat.objects.filter(fk_id_delivery__fk_id_delivery_man=current_user)
+
+    context = {
+        'chats':chats
+    }
+    return render(request, 'chats.html', context)
 
 def Conversation(request, id_chat):
+    user = request.user
+    current_user = UserProfile.objects.get(username=user)
     chat = get_object_or_404(Chat, pk=id_chat)
     messages = Message.objects.filter(fk_id_chat=chat)
-    return render(request, 'conversation.html', {'chat': chat, 'messages': messages})
+
+    context = {
+        'user':user,
+        'chat': chat,
+        'messages': messages
+        }
+
+    if request.method == 'GET':
+        if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
+            # Check if it's an AJAX request
+            messages_list = [
+                {
+                    'user': message.sender.username,
+                    'value': message.content,
+                    'date': message.timestamp.strftime("%b. %d, %Y, %I:%M %p")
+                }
+                for message in messages
+            ]
+            return JsonResponse({'messages': messages_list})
+        else:
+            # For regular page request, return the HTML page
+            return render(request, 'conversation.html', context)
+        
+    if request.method == 'POST':
+        print("hola")
+        content = request.POST.get('content')
+
+        # Create a new Message instance
+        message = Message.objects.create(
+            fk_id_chat=chat,
+            sender=current_user,
+            content=content,
+            timestamp=timezone.now()
+        )
+
+        # Update the last_update field of the associated chat
+        chat.last_update = timezone.now()
+        chat.save()
+
+        return JsonResponse({'success': True})
+
+
+    
+
+    return render(request, 'conversation.html',context)
