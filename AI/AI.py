6dac18f9615bd2ad2f5ae7b4eb17e24023c5b16e
Crissitx <camilocardenas2705@gmail.com>
2023-10-03 22:22:22 -0500

    Tesseract fully working

--- AI/AI.py
+++ AI/AI.py
@@ -1,46 +1,45 @@
+import sys
+sys.path.append("../Lib")
+sys.path.append("../pytesseract")
+
 import pytesseract
 from pytesseract import Output
-import PIL.Image
-import cv2
 import os
+import cv2
 
-config = r"--psm 11 --oem 3 -l spa"
+# If you don't have tesseract executable in your PATH, include the following:
+#pytesseract.pytesseract.tesseract_cmd = r'<full_path_to_your_tesseract_executable>'
+pytesseract.pytesseract.tesseract_cmd = r'AI\Tesseract-OCR\tesseract.exe'
 
-actualPath = os.path.dirname(os.path.abspath(__file__))
-#parentPath = os.path.dirname(actualPath)
-mediaPath = os.path.dirname(actualPath) + "\media" + "\AI"
-
-print("MEDIA PATH: " + mediaPath)
-
-imageName = input("Enter image name (Include the extension): ")
-if imageName == "":
-    # Use default image
-    imageName = "test.png"
-
-path = os.path.join(mediaPath, imageName)
+ai_confidence = 50
+confidence_reached_results = []
+path = "AI/media/test.png"
+config = r"--psm 6 --oem 3 -l spa"
+#config = r"--psm 6 --oem 3 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.,/()-&"
 
 img = cv2.imread(path)
 height, width, _ = img.shape
 
+# VISUALIZATION:
+#print(pytesseract.get_languages())
 data = pytesseract.image_to_data(img, config=config, output_type=Output.DICT)
-boxes = pytesseract.image_to_boxes(img, config=config)
-
-for i in range(len(data['text'])):
-    print(data['text'][i])
-
-for box in boxes.splitlines():
-    box = box.split(" ")
-    img = cv2.rectangle(img, (int(box[1]), height - int(box[2])), (int(box[3]), height - int(box[4])), (0,0,255), 1)
-
 amount_boxes = len(data['text'])
 for i in range(amount_boxes):
-    if float(data['conf'][i]) > 60:
+    if float(data['conf'][i]) > ai_confidence:
+        confidence_reached_results.append(data['text'][i])
         (x, y, width, height) = (data['left'][i], data['top'][i], data['width'][i], data['height'][i])
         img = cv2.rectangle(img, (x, y), (x + width, y + height), (0, 255, 0), 2)
-        img = cv2.putText(img, data['text'][i], (x, y+height+10), cv2.FONT_HERSHEY_PLAIN, 0.7, (255, 0, 0), 1, cv2.LINE_AA)
+        img = cv2.putText(img, data['text'][i], (x, y+height+10), cv2.FONT_HERSHEY_PLAIN, 0.8, (255, 0, 0), 1, cv2.LINE_AA)
 
-cv2.imshow("img", img)
-cv2.waitKey(0)
+#cv2.imshow("img", img)
+#cv2.waitKey(0)
+# VISUALIZATION ENDED
 
-text = pytesseract.image_to_string(PIL.Image.open(path), config=config)
-print("TEXT: " + text)
+# PRINT RESULTS WITH ENOUGH CONFIDENCE
+#print(confidence_reached_results)s
+result = ""
+for word in confidence_reached_results:
+    result += word + " "
+result.strip()
+
+print(result)
