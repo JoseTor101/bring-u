9870f6d876b661bf0b2d41830ad04c7238295cf8
josetor101 <josetordecilla800@gmail.com>
2023-10-18 07:06:55 -0500

    Imgs for dev-3

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-t45gl3e+n1v9-(8@$6&$p8d1)(uh25knwxz1pz5k101d+s1h5e
 DEBUG = True
 
 #ALLOWED_HOSTS = ['127.0.0.1','192.168.43.211']
-ALLOWED_HOSTS = ['192.168.49.173']
+ALLOWED_HOSTS = []
 
 
 # Application definition
